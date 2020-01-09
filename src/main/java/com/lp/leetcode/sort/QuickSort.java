a9a9907d424348060a8d46a9afb8e2140863d3a1
liangpeng <liangpeng@moqipobing.com>
2020-01-09 10:53:38 +0800

    update

--- src/main/java/com/lp/leetcode/sort/QuickSort.java
+++ src/main/java/com/lp/leetcode/sort/QuickSort.java
@@ -3,7 +3,7 @@ package com.lp.leetcode.sort;
 import java.util.Arrays;
 
 // 核心思想，选取数组的最后一个元素作为基准元素，循环与其他数组其它元素进行比较，如果符合条件（例如：小于基准元素的在基准元素的左边，大于的在右边）就交换位置
-public class QuickSort {有问题
+public class QuickSort {
 
     public static void main(String[] args) {
         int[] a = new int[]{100, 5, 8, 23, 67, 14, 23};
@@ -12,33 +12,7 @@ public class QuickSort {有问题
     }
 
     private static void quickSort(int[] a, int begin, int end) {
-        if (begin >= end) {
-            return;
-        }
-        int tmp = a[end];
-        int tmpIndex = end;
-        for (int i = begin; i < end; i++) {
-            if (tmp < a[i]) {
-                if (tmpIndex > i) {
-                    a[tmpIndex] = a[i];
-                    a[i] = tmp;
-                    tmpIndex = i;
-                }
-            } else if (tmp > a[i]) {
-                if (tmpIndex < i) {
-                    a[tmpIndex] = a[i];
-                    a[i] = tmp;
-                    tmpIndex = i;
-                }
-            }
-            System.out.println(Arrays.toString(a) + ";" + tmpIndex + ";" + i);
-        }
 
-        System.out.println(Arrays.toString(a) + ";" + tmpIndex + ";" + begin + ";" + end);
-
-        quickSort(a, begin, tmpIndex - 1);
-
-        quickSort(a, tmpIndex + 1, end);
 
 
     }
