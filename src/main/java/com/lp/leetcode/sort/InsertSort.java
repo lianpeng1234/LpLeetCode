e6c66042800604b29056a9c84eb53bf5f54a954a
liangpeng <liangpeng@moqipobing.com>
2021-10-10 20:29:30 +0800

    update

--- src/main/java/com/lp/leetcode/sort/InsertSort.java
+++ src/main/java/com/lp/leetcode/sort/InsertSort.java
@@ -2,22 +2,20 @@ package com.lp.leetcode.sort;
 
 public class InsertSort {
 
-    // 代码中演示的排序为 降序（从小到大）
+    // 排序后的数组 降序排列
     public void sort(int[] nums) {
-        int orderlyEndIndex = 0;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[orderlyEndIndex] > nums[i]) {// 已排序区间 最后一个元素 比 未排序区间的 第一个元素大
-                for (int j = i; j > 0; j--) {// 从已排序区间末尾开始向前遍历
-                    if (nums[j] < nums[j - 1]) { // 遍历到的元素如果比 X 大则交换位置
-                        int tmp = nums[j];
-                        nums[j] = nums[j-1];
-                        nums[j-1] = tmp;
-                    }
+            int tmp = nums[i];// 临时记录 未排序区间头部元素
+            int j = i;// 记录插入的数组下标
+            for (; j > 0; j--) {// 从已排序区间末尾开始向前遍历
+                if (tmp < nums[j - 1]) {
+                    nums[j] = nums[j - 1];
+                } else {
+                    break;
                 }
             }
-            orderlyEndIndex = i;// 已排序区间的末尾临界数组下标完后移动到 i
+            nums[j] = tmp;
         }
     }
-
 
 }
