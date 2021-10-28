3cb4f3ac79becff45c04895a84770a31306b9351
liangpeng <liangpeng@moqipobing.com>
2021-10-28 19:38:26 +0800

    leetcode

--- src/main/java/com/lp/leetcode/other/LeetCode179.java
+++ src/main/java/com/lp/leetcode/other/LeetCode179.java
@@ -7,7 +7,7 @@ import java.util.PriorityQueue;
 public class LeetCode179 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3, 30, 34, 5, 9};
+        int[] nums = new int[]{34323, 3432};
         largestNumber(nums);
     }
 
@@ -16,22 +16,12 @@ public class LeetCode179 {
         PriorityQueue<String> queue = new PriorityQueue<>(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length() < o2.length()) {
-                    StringBuilder sb = new StringBuilder(o1);
-                    for (int i = 0; i < o2.length() - o1.length(); i++) {
-                        sb.append(o1.charAt(o1.length() - 1));
-                    }
-                    o1 = sb.toString();
+                if (o1.length() == o2.length()) {
+                    return o2.compareTo(o1);
                 }
-                if (o1.length() > o2.length()) {
-                    StringBuilder sb = new StringBuilder(o2);
-                    for (int i = 0; i < o1.length() - o2.length(); i++) {
-                        sb.append(o2.charAt(o2.length() - 1));
-                    }
-                    o2 = sb.toString();
-                }
-
-                return o2.compareTo(o1);
+                String tmp1 = o1 + o2;
+                String tmp2 = o2 + o1;
+                return tmp2.compareTo(tmp1);
             }
         });
 
@@ -39,11 +29,21 @@ public class LeetCode179 {
             queue.offer(String.valueOf(nums[i]));
         }
 
+        boolean flag = true;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < nums.length; i++) {
-            sb.append(queue.poll());
+            String str = queue.poll();
+            if ("0".equals(str) && flag) {
+                continue;
+            }
+            flag = false;
+            sb.append(str);
+            System.out.println(str);
         }
 
+        if(sb.toString().length() < 1) {
+            return "0";
+        }
         return sb.toString();
     }
 }
